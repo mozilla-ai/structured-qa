2cb41eb73a8fc56f2057bccafbdb1a426f88c35a
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2025-02-05 17:02:34 +0100

    5 add benchmark (#16)
    * enh(preprocessing): Add split_markdown_by_headings.
    
    * Add benchmark
    
    * Move to structured_qa. Add entrypoint
    
    * Move back outside
    
    * Fix main
    
    * Update questions
    
    * Update model and prompt
    
    * Update
    
    * Update
    
    * fix
    
    * Add system_instruction
    
    * Update ratio
    
    * Add more wait
    
    * Fix return
    
    * Fix URLs
    
    * Update download name
    
    * Update
    
    * Update
    
    * Update with upper
    
    * Cast to str
    
    * Extend
    
    * Add benchmark
    
    * Fix
    
    * fix
    
    * Drop export
    
    * Updates
    
    * Update default model
    
    * Update
    
    * Use info
    
    * Update with None
    
    * Add answer type
    
    * Refactor
    
    * Add fallback for out of context
    
    * Update with debugging info
    
    * Update
    
    * Update with mit-1
    
    * test unsloth
    
    * Add , skip_special_tokens = True
    
    * Update
    
    * Updates
    
    * Add full_context
    
    * Update full context
    
    * update
    
    * Add load and clean
    
    * Update
    
    * Update
    
    * print
    
    * Update
    
    * Add load_gemini_model
    
    * Add sleep
    
    * Update get_response
    
    * Update
    
    * Log error
    
    * fix
    
    * Make the more info check more flexible
    
    * Add gemini_full_context notebook
    
    * typo
    
    * Check por API KEY
    
    * Update with outputs
    
    * Add ragatouille
    
    * Fix
    
    * Update notebooks
    
    * Update gemini notebooks
    
    * Extend structured_qa. Add perfect_context.
    
    * Add gemini_perfect_context
    
    * Update
    
    * fix line
    
    * fix line
    
    * Update perfect_context
    
    * Add missing perfect context
    
    * Updates
    
    * Update gemini_ragatouille
    
    * Update gemini_fra
    
    * Update
    
    * Update
    
    * Drop some log
    
    * Update
    
    * Update gemini_perfect_context with results
    
    * Use rapizfuzz
    
    * Use question_part
    
    * Fix
    
    * break when no section_names
    
    * Update prompt
    
    * Add qwen perfect context
    
    * Update gemini_find_retrieve_answer
    
    * Update qwen perfect context
    
    * Add qwen RAGatouille
    
    * Update qwen notebooks
    
    * Update
    
    * Update prompt
    
    * Update qwen notebooks
    
    * Cleanup
    
    * Cleanup
    
    * Add DeepSeek-R1-Distill-Qwen-7B
    
    * Debug current calls. Set to 9 before reset
    
    * Add qwen find retrieve answer
    
    * Extend benchmark
    
    * Update
    
    * Add max_sections_to_check
    
    * Default to None
    
    * Default to half of sections
    
    * Update
    
    * fix
    
    * Fix
    
    * Add qwen full context
    
    * Update qwen_full_context
    
    * Update gemini_full_context
    
    * Add statistics
    
    * Update prompt
    
    * Update with type
    
    * Update gemini prompt and count
    
    * Update results with same prompts
    
    * Update with same prompt
    
    * Update results
    
    * Bring back llama-cpp-python
    
    * Update prompts
    
    * Reduce notebook size
    
    * Update pre-commit
    
    * Update docstrings
    
    * Update test

--- src/structured_qa/preprocessing.py
+++ src/structured_qa/preprocessing.py
@@ -17,17 +17,7 @@ def split_markdown_by_headings(
         heading_patterns (str, optional): A list of regex patterns representing heading markers
             in the markdown document.
             Defaults to None.
-            If None, the default patterns are used:
-
-            ```python
-            [
-                r"^#\s+(.+)$",
-                r"^##\s+(.+)$",
-                r"^###\s+(.+)$",
-                r"^\*\*[\d\.]+\.\*\*\s*\*\*(.+)\*\*$",
-                r"^\*\*[\d\.]+\.\*\*\s+(.+)$"
-            ]
-            ```
+            If None, the default patterns are used.
 
     Returns:
         dict[str, str]: A dictionary where the keys are the section names and the values are the section contents.
@@ -92,7 +82,6 @@ def document_to_sections_dir(input_file: str, output_dir: str) -> list[str]:
         md_text,
     )
     logger.success(f"Found {len(sections)} sections")
-
     logger.info(f"Writing sections to {output_dir}")
     output_dir = Path(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
--- src/structured_qa/workflow.py
+++ src/structured_qa/workflow.py
@@ -1,23 +1,32 @@
 from pathlib import Path
 
-
-from llama_cpp import Llama
 from loguru import logger
+from rapidfuzz import process
+
+from structured_qa.model_loaders import LlamaModel
+
+
+def get_matching_section(response, section_names):
+    """
+    Use string similarity to find the most similar section_name.
+    """
+    return process.extractOne(response, section_names)[0]
 
 
 def find_retrieve_answer(
     question: str,
-    model: Llama,
+    model: LlamaModel,
     sections_dir: str,
     find_prompt: str,
     answer_prompt: str,
+    max_sections_to_check: int | None = None,
 ) -> tuple[str, list[str]] | tuple[None, list[str]]:
     """
     Workflow to find the relevant section, retrieve the information, and answer the question.
 
     Args:
         question (str): The question to answer.
-        model (Llama): The Llama model to use for generating completions.
+        model (LlamaModel): The model to use for generating completions.
         sections_dir (str): The directory containing the sections.
             See [`document_to_sections_dir`][structured_qa.preprocessing.document_to_sections_dir].
             Structure of the sections directory:
@@ -34,6 +43,9 @@ def find_retrieve_answer(
         answer_prompt (str): The prompt for answering the question.
 
             See [`ANSWER_PROMPT`][structured_qa.config.ANSWER_PROMPT].
+        max_sections_to_check (int, optional): The maximum number of sections to check before giving up.
+            Defaults to None.
+            If None, it will check  up to a maximum of 20 sections until it finds the answer.
 
     Returns:
         tuple[str, list[str]] | tuple[None, list[str]]:
@@ -46,18 +58,22 @@ def find_retrieve_answer(
     current_info = None
     current_section = None
 
+    if max_sections_to_check is None:
+        max_sections_to_check = min(20, len(sections_names))
+
     sections_checked = []
-    while True:
+    while len(sections_checked) <= max_sections_to_check:
         logger.debug(f"Current information available: {current_info}")
         if not current_info:
             logger.debug("Finding section")
             finding_section = True
+            question_part, *options = question.split("?")
             messages = [
                 {
                     "role": "system",
                     "content": find_prompt.format(SECTIONS="\n".join(sections_names)),
                 },
-                {"role": "user", "content": question},
+                {"role": "user", "content": question_part},
             ]
         else:
             logger.debug("Answering question")
@@ -70,26 +86,29 @@ def find_retrieve_answer(
                 {"role": "user", "content": question},
             ]
 
-        result = model.create_chat_completion(messages)
-        result = result["choices"][0]["message"]["content"]
-
-        logger.debug(f"Result: {result}")
+        try:
+            response = model.get_response(messages)
+        except Exception as e:
+            logger.error(f"Failed to generate completion: {e}")
+            return "Generation Error", sections_checked
 
         if finding_section:
-            result = result.strip()
-            logger.info(f"Retrieving section: {result}")
-            if result in sections_names:
-                section_content = (sections_dir / f"{result}.txt").read_text()
-                current_section = result
-                current_info = section_content
-                sections_checked.append(result)
-            else:
-                logger.error(f"Unknown section: {result}")
-                return None, sections_checked
+            response = response.strip()
+            if not sections_names:
+                return "NOT FOUND", sections_checked
+            section_name = get_matching_section(response, sections_names)
+            logger.debug(f"Retrieving section: {section_name}")
+            section_content = (sections_dir / f"{section_name}.txt").read_text()
+            current_section = section_name
+            current_info = section_content
+            sections_checked.append(section_name)
+
         else:
-            if result == "I need more info.":
+            if "MORE INFO" in response.upper():
                 current_info = None
                 sections_names.remove(current_section)
                 continue
             else:
-                return result, sections_checked
+                return response, sections_checked
+
+    return "NOT FOUND", sections_checked
